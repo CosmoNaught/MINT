b439ff904889a1e7db1062f79caf2e62c6380136
CosmoNaught <santonicosmo@gmail.com>
2024-07-17 08:44:57 +0100

    Fix main control

--- run/main.R
+++ run/main.R
@@ -4,8 +4,9 @@ t0 <- Sys.time()
 id <- orderly2::orderly_run("bednet_param_gen")
 id <- orderly2::orderly_run("param_sampling", list(run = "short_run"))
 id <- orderly2::orderly_run("param_sampling_plots", list(run = "short_run"))
-id <- orderly2::orderly_run("simulation_prep", list(run = "short_run", num_sample = 5, run_control = TRUE, debug_plot = TRUE))
+id <- orderly2::orderly_run("simulation_prep", list(run = "short_run", num_sample = 5, run_control = TRUE, plot = TRUE))
 print(Sys.time() - t0)
 
 ## Main functions for execution can be found here
 ## See cluster.R for running on cluster
+## cluster-integration
