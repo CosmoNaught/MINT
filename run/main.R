b689ea0710d916440fcacbc9fde1ae25fcd5a9be
CosmoNaught <santonicosmo@gmail.com>
2024-07-23 17:08:43 +0100

    Remove redundant file and add functionality for spearMINT package

--- run/main.R
+++ run/main.R
@@ -1,5 +1,8 @@
 ## non-cluster setup
 ## can't load paramter specific dependency
+devtools::install_local("~/Documents/Imperial/PhD/Projects/spearMINT", force = TRUE)
+devtools::load_all("~/Documents/Imperial/PhD/Projects/spearMINT")
+
 t0 <- Sys.time()
 # id <- orderly2::orderly_run("bednet_param_gen")
 id <- orderly2::orderly_run("collate_bednet_param")
--- src/collate_bednet_param/collate_bednet_param.R
+++ src/collate_bednet_param/collate_bednet_param.R
@@ -1,23 +1,15 @@
 # Load necessary libraries
-library(dplyr)
-
-# Function to read CSV and apply transformations
-load_itn_data <- function(file_path, net_type) {
-  data <- read.csv(file_path)
-  data <- data %>%
-    mutate(net_type = net_type) %>%
-    rename(dn0 = ERG_d_ITN0, rn0 = ERG_r_ITN0) %>%
-    select(dn0, rn0, gamman, bioassay_surv, net_type)
-  return(data)
-}
-
-# Load the data
-D1load <- load_itn_data("pyrethroid_only_median_july2024.csv", "pyrethroid_only")
-D2load <- load_itn_data("pyrethroid_pbo_median_july2024.csv", "pyrethroid_pbo")
-D3load <- load_itn_data("pyrethroid_pyrrole_median_july2024.csv", "pyrethroid_pyrrole")
+library(spearMINT)
+browser()
+# Define file paths
+file_paths <- list(
+  "pyrethroid_only" = "pyrethroid_only_median_july2024.csv",
+  "pyrethroid_pbo" = "pyrethroid_pbo_median_july2024.csv",
+  "pyrethroid_pyrrole" = "pyrethroid_pyrrole_median_july2024.csv"
+)
 
 # Combine the datasets
-ITN_median_param <- bind_rows(D1load, D2load, D3load)
+ITN_median_param <- spearMINT::combine_itn_data(file_paths)
 
 # Store dataframe
-saveRDS(ITN_median_param, "bednet_params_raw.RDS")
+spearMINT::save_itn_data(ITN_median_param, "bednet_params_raw.RDS")
