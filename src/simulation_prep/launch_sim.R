95618862b172316f3a04b42e97f6362622f85ad1
CosmoNaught <santonicosmo@gmail.com>
2024-08-05 15:14:13 +0100

    Massive refactor boiiii

--- src/simulation_prep/launch_sim.R
+++ src/simulation_prep/launch_sim.R
@@ -1,32 +1,3 @@
-# run_sim_with_reps <- function(
-#     timesteps,
-#     parameters = list(),
-#     repetitions,
-#     parallel = FALSE
-# ) {
-#   if (parallel) {
-#     print("Repetition sequence: parallel")
-#     fapply <- parallel::mclapply
-#   } else {
-#     print("Repetition sequence: sequential")
-#     fapply <- lapply
-#   }
-  
-#   # Use a list to store each repetition's dataframe independently
-#   list_dfs <- fapply(
-#     seq(repetitions),
-#     function(repetition) {
-#       df <- malariasimulation::run_simulation(timesteps, parameters)
-#       df$repetition <- repetition
-#       return(df)
-#     }
-#   )
-  
-#   # Create a named list where each key corresponds to the repetition number
-#   names(list_dfs) <- paste0("repetition_", seq(repetitions))
-#   return(list_dfs)
-# }
-
 library(malariasimulation)
 
 # Define the simulation function outside of the main function
