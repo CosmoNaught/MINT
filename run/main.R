e6fec402159e44e92387640663080b46df6f7f91
CosmoNaught <santonicosmo@gmail.com>
2024-12-19 14:34:01 +0000

    improve sampling scheme, plotting and execution script

--- run/main.R
+++ run/main.R
@@ -10,16 +10,18 @@ hipercow::task_log_watch(id1)
 
 ############################ Launch parameter space explorer ############################
 
+# Local
+
+id2 <- orderly2::orderly_run("param_sampling", 
+list(run = "long_run", gen_grid = FALSE))
+
+# Cluster
+
 id2 <- hipercow::task_create_expr(orderly2::orderly_run("param_sampling", 
 list(run = "long_run",
 local_cluster = FALSE)),
 resources = hipercow::hipercow_resources(cores = 32))
 hipercow::task_log_watch(id2)
-
-############################ Plot parameter space ############################
-
-# DISABLED FOR NOW TO BE FIXED LATER
-# id3 <- hipercow::task_create_expr(orderly2::orderly_run("param_sampling_plots", list(run = "short_run")))
 
 ############################ Launch simulation setup ############################
 
@@ -30,18 +32,46 @@ library(dplyr)
 param_index <- 1
 reps <- 1
 
-for (i in seq(1, parameter_set)){
+for (i in seq(1, param_index)){
     orderly2::orderly_run("simulation_controller",
     list(run = "long_run",
-                    param_index = parameter_set,
+                    param_index = param_index,
                     reps = reps,
                     rrq = FALSE))
 }
 
-# Cluster
+# Cluster Testing
 library(tibble)
 library(dplyr)
-param_index <- 1024
+param_index <- 4#2^14
+reps <- 2
+
+r <- hipercow::hipercow_rrq_controller()
+
+  tid <- hipercow::task_create_expr({
+          orderly2::orderly_run(
+          "simulation_controller",
+          list(
+              run = "long_run",
+              param_index = param_index,
+              reps = reps,
+              rrq = TRUE
+          )
+      )
+  },
+  parallel = hipercow::hipercow_parallel(use_rrq = TRUE),
+  resources = hipercow::hipercow_resources(queue = "Testing")
+  )
+
+info <- hipercow::hipercow_rrq_workers_submit(n = 4, 
+resources = hipercow::hipercow_resources(queue = "Testing"))
+hipercow::task_log_watch(tid)
+
+
+# Cluster FullDeployment
+library(tibble)
+library(dplyr)
+param_index <- 2^14
 reps <- 8
 
 r <- hipercow::hipercow_rrq_controller()
@@ -57,10 +87,12 @@ r <- hipercow::hipercow_rrq_controller()
           )
       )
   },
-  parallel = hipercow::hipercow_parallel(use_rrq = TRUE)
+  parallel = hipercow::hipercow_parallel(use_rrq = TRUE),
+  resources = hipercow::hipercow_resources(queue = "AllNodes")
   )
 
-info <- hipercow::hipercow_rrq_workers_submit(128)
+info <- hipercow::hipercow_rrq_workers_submit(256,
+resources = hipercow::hipercow_resources(queue = "AllNodes"))
 hipercow::task_log_watch(tid)
 
 
@@ -70,41 +102,4 @@ orderly2::orderly_run(
         "simulation_plots"
     )
 
-
 ####################################################################
-
-load_simulation_results <- function(task_id) {
-  base_dir <- "/home/ye120/net/malaria/Cosmo/MINT/archive/simulation_controller"
-  file_path <- file.path(base_dir, task_id, "simulation_results.rds")
-  
-  if (!file.exists(file_path)) {
-    stop("The specified task ID does not exist or the file could not be found: ", file_path)
-  }
-  
-  simulation_results <- readRDS(file_path)
-  
-  return(simulation_results)
-}
-
-###################################################################
-
- library(tibble)
- 
-    # Example input data
-    input_data <- tst$input
- 
-    # Function to dynamically collapse nested lists into a single tibble row
-    collapse_to_tibble <- function(data) {
-      data$treatment_timesteps$mass_bednet <- list(data$treatment_timesteps$mass_bednet)
-      data$treatment_timesteps$irs <- list(data$treatment_timesteps$irs)
-      
-      flattened <- unlist(data, recursive = FALSE)
-      names(flattened) <- gsub("\\.", "_", names(flattened))
-      as_tibble(flattened)
-    }
- 
-    # Apply function to input data
-    result <- collapse_to_tibble(input_data)
- 
-    # Print the resulting tibble
-    print(result)
